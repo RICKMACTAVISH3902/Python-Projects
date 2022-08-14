8648656f7e55ebe7a82af7404cf9f8bffce89807
ARITRA DEY <111201938+RICKMACTAVISH3902@users.noreply.github.com>
2022-08-15 02:59:40 +0530

    Update Graph(y=f(x)).py

--- Graph(y=f(x)).py
+++ Graph(y=f(x)).py
@@ -1,5 +1,4 @@
 # Graph Plotting of Functions
-import math as mt
 
 import matplotlib.pyplot as plt
 import math as mt
@@ -17,21 +16,3 @@ for i in range(np):
 print(y)
 plt.plot(xx, yy)
 plt.show()
-
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
